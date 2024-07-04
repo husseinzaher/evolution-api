ac36a542bfb01db6c96890fab5dda9c2f91f0a43
Hussein Zaher <husseinzaher@outlook.com>
2024-07-04 19:13:34 +0300

    chore: add maxListeners to event emmiter

--- src/config/event.config.ts
+++ src/config/event.config.ts
@@ -3,5 +3,6 @@ import EventEmitter2 from 'eventemitter2';
 export const eventEmitter = new EventEmitter2({
   delimiter: '.',
   newListener: false,
-  ignoreErrors: false,
+  ignoreErrors: true,
+  maxListeners: 0,
 });
