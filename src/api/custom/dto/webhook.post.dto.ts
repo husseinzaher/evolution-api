a8449c682d99be8b7a1231b8d8abe4d0c15de01f
Hussein Zaher <husseinzaher@outlook.com>
2024-07-08 23:42:01 +0300

    chore: add custom wehbhook

--- src/api/custom/dto/webhook.post.dto.ts
+++ src/api/custom/dto/webhook.post.dto.ts
@@ -3,11 +3,12 @@ import { Events } from '../../types/wa.types';
 export class WebhookPostDto {
   event: Events;
   instance: string;
-  data: any;
-  destination: string;
-  date_time: string;
-  sender: string;
-  server_url: string;
+  data?: any;
+  statusReason?:string;
+  destination?: string;
+  date_time?: string;
+  sender?: string;
+  server_url?: string;
   apikey?: string;
   from?: string;
 }
