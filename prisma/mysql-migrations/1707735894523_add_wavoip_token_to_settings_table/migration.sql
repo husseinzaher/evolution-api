cb3645d450f4cd0745e6ad3288190a08419686fd
Hussein Zaher <husseinzaher@outlook.com>
2025-07-24 03:30:40 +0300

    chore: add avatar to messages

--- prisma/mysql-migrations/1707735894523_add_wavoip_token_to_settings_table/migration.sql
+++ prisma/mysql-migrations/1707735894523_add_wavoip_token_to_settings_table/migration.sql
@@ -4,6 +4,19 @@
   - A unique constraint covering the columns `[remoteJid,instanceId]` on the table `Chat` will be added. If there are existing duplicate values, this will fail.
 */
 
--- AlterTable
-ALTER TABLE `Setting`
-  ADD COLUMN IF NOT EXISTS `wavoipToken` VARCHAR(100);
+SELECT COUNT(*)
+INTO @col_exists
+FROM information_schema.columns
+WHERE table_schema = DATABASE()
+  AND table_name   = 'Setting'
+  AND column_name  = 'wavoipToken';
+
+SET @ddl = IF(
+  @col_exists = 0,
+  'ALTER TABLE `Setting` ADD COLUMN `wavoipToken` VARCHAR(100);',
+  'SELECT ''Column already exists'';'
+);
+
+PREPARE stmt FROM @ddl;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
